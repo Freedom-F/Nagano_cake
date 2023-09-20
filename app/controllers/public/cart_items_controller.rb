7a099874513135f9f1d7f777b66e9f11ca961446
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-09-20 11:31:01 +0000

    Fix商品機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,7 +2,7 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
-    @total_price = @cart_items.sum { |cart_item| cart_item.subtotal }
+    #@total_price = @cart_items.sum { |cart_item| cart_item.subtotal }
   end
 
   def update
@@ -15,14 +15,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_items = CartItem.new(cart_items_params)
-    @cart_items.save
-      redirect_to public_cart_items_path
+    @cart_item = CartItem.new(cart_items_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = params[:cart_item][:item_id]
+    @cart_item.save
+      redirect_to public_cart_items_index_path
   end
 
   private
-  def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+  def cart_items_params
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 
 end
