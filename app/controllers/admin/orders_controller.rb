9daff8181c3b86cf5c174b310cb8e94c285b2207
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-09-23 15:17:47 +0900

    [Add] 注文ステータス更新機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,16 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
-    # byebug
     order = Order.find(params[:id])
-    if order.update(order_params)
-      redirect_to admin_order_path
-    else
-      byebug
-    end
+    
+    order.update(order_params)
+    redirect_to admin_order_path
   end
   
   def order_params
