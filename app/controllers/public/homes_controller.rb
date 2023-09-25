8c4f32884fe2f2c23dcd26cce433586d826aa135
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-09-25 09:23:26 +0000

    Fix

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,6 +4,7 @@ class Public::HomesController < ApplicationController
     render 'top'
     @new_items = Item.new_arrivals
     @new_items = Item.where(is_new: true).order(created_at: :desc)
+    @new_items = Item.order(created_at: :desc).limit(5)
   end
 
   def about
